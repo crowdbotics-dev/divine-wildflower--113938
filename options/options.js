76cd8bddc03c927629f56e62a5212a7378acad75
Crowdbotics <engineering@crowdbotics.com>
2023-09-03 23:11:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-wildflower--113938",
+  url: "https://divine-wildflower--113938.botics.co",
+  api: "https://divine-wildflower--113938.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
